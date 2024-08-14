6d4b1f5fc9b3a7b0cbf10fba2b56a72f4de53b3c
Dubb74 <gdubb2746@gmail.com>
2024-08-14 09:37:27 -0700

    nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 
 import './App.css';
-
+import img1 from "./img/logo.jpg"
 function App() {
   return (
    <>
-   Hello
+    <div id="nav">
+      
+    <img  className="pic1"src={img1} alt="pic"/>
+    
+        <div>
+            <h1>W.Howard</h1>
+        </div>
+        
+        <div> web Developer</div>
+        <a href='About' className='nav-link'>About</a>
+        <a href='Skills' className='nav-link'>Skills</a>
+        
+   
+        <a href='project' className='nav-link'>Project 
+        </a>
+        <a href='contact' className='nav-link'>Contact
+        </a>
+        <a href='Linkedin' className='nav-link'><i class="fa-brands fa-linkedin"></i></a>
+
+        <a href='github' className='nav-link'><i class="fa-brands fa-square-github"></i>
+        </a>
+        <a href='email' className='nav-link'><i class="fa-regular fa-envelope"></i>
+        </a>
+ </div>
+
    </>
   );
 }
