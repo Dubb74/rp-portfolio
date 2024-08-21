ff7034ae426f312774235baea9fcaeafd27d7f68
Dubb74 <gdubb2746@gmail.com>
2024-08-21 10:38:32 -0700

    email input

--- src/views/project.jsx
+++ src/views/project.jsx
@@ -4,9 +4,28 @@ import pic2 from "./img2/profile.jpg"
 import pic3 from "./img2/survey copy.jpg"
 import pic4 from "./img2/technical.jpg"
 import pic5 from "./img2/tribute.jpg"
-
+import { useState,useRef } from "react"
+import emailjs from "@emailjs/browser";
 
 const Project = () => {
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_u9t1y9p", "template_g4k4z9a", form.current, {
+        publicKey: "MO0meLyMFB79rwywe",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          form.current.reset();
+          setName("");
+          setEmail("");
+          setMessage("")
+  })}
   return (
     <>
       <section id="projects">
@@ -20,21 +39,64 @@ const Project = () => {
       <br />
       <section id="prof">
         <div className="img5">
-          <img src={pic} alt="" class="picsiz" />
-          <img src={pic1}alt="" class="picsiz" />
-          <img src={pic2} alt="" class="picsiz" />
+          <img src={pic} alt="" className="picsiz" />
+          <img src={pic1}alt="" className="picsiz" />
+          <img src={pic2} alt="" className="picsiz" />
         </div>
         <div className="img5">
-          <img src={pic3} alt="" class="picsiz" />
-          <img src={pic4} alt="" class="picsiz" />
+          <img src={pic3} alt="" className="picsiz" />
+          <img src={pic4} alt="" className="picsiz" />
           <img
             src={pic5}
             alt=""
-            class="
+            className="
        picsiz"
           />
         </div>
       </section>
+        
+        <form ref={form} onSubmit={sendEmail} className="elabel">
+        <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+                className="input"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                className="input"
+                required
+              />
+              
+              <textarea
+                name="message"
+                placeholder="Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className="textarea"
+                rows="4"
+                required
+              />
+              <button type="submit" className="btn border-none self-center">
+                Send
+              </button>
+
+
+
+
+
+
+
+
+       
+          </form>
     </>
   );
 };
